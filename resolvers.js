a63c69605c303d00152aca0a95df651b5e4146f4
Dmytro Didukh <jeriko.outback@gmail.com>
2020-09-17 22:14:34 +0300

    implementing stage 1.5

--- resolvers.js
+++ resolvers.js
@@ -1,18 +1,12 @@
 import {categoryQuery, categoryMutation} from './modules/category/category.resolver';
-import {subcategoryQuery, subcategoryMutation} from './modules/subcategory/subcategory.resolver';
 import {productQuery, productMutation} from './modules/product/product.resolver';
 import {orderQuery, orderMutation} from './modules/order/order.resolver';
 import {userQuery, userMutation} from './modules/user/user.resolver';
 import {bannerQuery, bannerMutation} from './modules/banner/banner.resolver';
 
-import CategoryService from "./modules/category/category.service";
-import SubcategoryService from "./modules/subcategory/subcategory.service";
-import ProductService from "./modules/product/product.service";
-
 const resolvers = {
     Query: {
         ...categoryQuery,
-        ...subcategoryQuery,
         ...productQuery,
         ...orderQuery,
         ...userQuery,
@@ -20,22 +14,10 @@ const resolvers = {
     },
     Mutation: {
         ...categoryMutation,
-        ...subcategoryMutation,
         ...productMutation,
         ...orderMutation,
         ...userMutation,
         ...bannerMutation,
-    },
-    Category: {
-        subcategories: (parent) => SubcategoryService.getSubcategoriesByCategoryId(parent.id),
-    },
-    Subcategory: {
-        category: (parent) => CategoryService.getCategoryById(parent.categoryId),
-    },
-    Product: {
-        category: (parent) => CategoryService.getCategoryById(parent.categoryId),
-        subcategory: (parent) => SubcategoryService.getSubcategoryById(parent.subcategoryId),
-    }
-};
+    }};
 
 export default resolvers;
