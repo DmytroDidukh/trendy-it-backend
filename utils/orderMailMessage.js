1ce87b608264ec657edb12d1aecfc57e2b6d83f0
Dmytro Didukh <jeriko.outback@gmail.com>
2020-09-22 01:48:58 +0300

    -order mail styles

--- utils/orderMailMessage.js
+++ utils/orderMailMessage.js
@@ -1,27 +1,126 @@
 const orderMailMessage = (order) => {
-        console.log(order)
-        return `
-<div style="
-background-color:  #3F3F3F;
-height: 450px;
-padding-top: 100px;
-">
-  <div style="
-  background-color: white;
-  width: 553px;
-  height: 140px;
-  margin: 28px 88px;
-  padding: 52px 45px;
-  ">
-    <p style="
-    margin: 0px
-    ">
-      Замовлення №: ${order.orderId}
-    </p>
-    <p style="margin-bottom: 45px">blah blah</p>
-    <p>З найкращими побажаннями, команда Trendy IT</p>
-  </div>
-</div>
+    const COLORS_DATA = {
+        black: {hex: '#000000'},
+        silver: {hex: '#6c757d'},
+        white: {hex: '#ffffff'},
+        blue: {hex: '#007bff'},
+        yellow: {hex: '#ffc107'},
+        orange: {hex: '#fd7e14'},
+        red: {hex: '#dc3545'},
+        green: {hex: '#28a745'},
+        pink: {hex: '#e83e8c'},
+        purple: {hex: '#6f42c1'},
+        brown: {hex: '#692e19'},
+    };
+
+    const products = order.products.map((product, i) => (
+        `<tr>
+           <td style='border: 1px solid #333; text-align: center;'>${i + 1}</td>
+           <td style='border: 1px solid #333;' >${product.name}</td>
+           <td style='border: 1px solid #333; text-align: center;'>
+                <span style='background-color: ${COLORS_DATA[product.color].hex}; 
+                                display: inline-block;
+                                width: 20px;
+                                height: 20px;
+                                border: 1px solid #f1f1f1;
+                                border-radius: 50%;'/>
+           </td>
+           <td style='border: 1px solid #333; text-align: center;'>${product.quantity}</td>
+           <td style='border: 1px solid #333; text-align: center;'>${product.price}</td>
+        </tr>`
+    ))
+
+    const delivery = () => {
+       switch (order.delivery.method) {
+           case 'кур‘єром': {
+               return `<p>Адреса: <span style='font-weight: bold; margin-left: 5px;'> ${order.delivery.city},
+                                        вул. ${order.delivery.address.street},
+                                        буд. ${order.delivery.address.built}
+                                ${order.delivery.address.apartment ?
+                                `<span style='font-weight: bold; margin-left: 5px;'>, кв. ${order.delivery.address.apartment} </span>` : ''}
+                                   </span> 
+                     </p>`
+           }
+           case 'на відділення Нової Пошти': {
+               return `<p>Адреса:
+                            <span style='font-weight: bold; margin-left: 5px;'>${order.delivery.city}, відділення ${order.delivery.postOffice}</span>
+                        </p>`
+           }
+       }
+    }
+
+
+    return `
+    <div style='border: 1px solid silver;
+                margin: 50px auto;
+                border-radius: 20px;
+                background-color: #e0e0e0;
+                padding: 50px;
+                width: 500px;
+                box-sizing: content-box;'>
+            <h3 style='text-align: center;'>Замовлення №: ${order.orderId}</h3>
+            <hr/>
+            <div>
+                <p>Покупець: <span style='font-weight: bold; margin-left: 5px;'>${order.customer.surname}, ${order.customer.name}</span></p>
+                <p>Ел. пошта: <span style='font-weight: bold; margin-left: 5px;'>${order.customer.email}</span></p>
+                <p>Телефон: <span style='font-weight: bold; margin-left: 5px;'>${order.customer.phone}</span></p>
+                <br/>
+                <p>Метод зв‘язку: <span style='font-weight: bold; margin-left: 5px;'>${order.connectionMethod}</span></p>
+                <p>Спосіб доставки: <span style='font-weight: bold; margin-left: 5px;'>${order.delivery.method}</span></p>
+                ${delivery()}         
+                <hr/>
+                <table style='width: 100%;'>
+                    <thead>
+                    <tr>
+                        <th style='border: 1px solid #333;
+                                    color: white;
+                                    background: #4a4a4a;
+                                    text-align: center;'>#</th>
+                        <th style='border: 1px solid #333;
+                                    color: white;
+                                    background: #4a4a4a;
+                                    text-align: center;'>Товар</th>
+                        <th style='border: 1px solid #333;
+                                    color: white;
+                                    background: #4a4a4a;
+                                    text-align: center;'>Колір</th>
+                        <th style='border: 1px solid #333;
+                                    color: white;
+                                    background: #4a4a4a;
+                                    text-align: center;'>Кількість</th>
+                        <th style='border: 1px solid #333;
+                                    color: white;
+                                    background: #4a4a4a;
+                                    text-align: center;'>Ціна за шт.</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        ${products}
+                    </tbody>
+                </table>
+                <hr/>
+                <p style='text-align: right;
+                          margin: 10px;
+                          font-size: 16px;'>
+                          Сума: <span style='font-weight: bold; margin-left: 5px;'>${order.products.reduce((sum, item) => sum + item.quantity * item.price, 0)} UAH</span></p>
+                <hr/>
+                <p>Наш менеджер зв‘яжеться з Вами у зручний для Вас спосіб.</p>
+                <h4>З найкращими побажаннями, команда Trendy IT 
+                    <span style='background: #009344;
+                       display: inline-block;
+                       width: 8px;
+                       height: 13px;'/>
+                      <span style='background: #ffffff;
+                                display: inline-block;
+                                 width: 8px;
+                                 height: 13px;'/>
+                      <span style='background: #cf2734;
+                                    display: inline-block;
+                                    width: 8px;
+                                    height: 13px;'/>
+                 </h4>
+            </div>
+        </div>
 `;
 };
 
