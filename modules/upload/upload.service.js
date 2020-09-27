ac1e4b102548943b8d73c1f1fc46fcb7f6a77c24
Dmytro Didukh <jeriko.outback@gmail.com>
2020-09-27 14:22:41 +0300

    upload changes

--- modules/upload/upload.service.js
+++ modules/upload/upload.service.js
@@ -3,27 +3,15 @@ import cloudinary from '../../utils/cloudinary';
 
 class UploadService {
     async uploadFile(file) {
-        try {
-            return await cloudinary.v2.uploader.upload(
-                file, {
-                    folder: "trendy-it"
-                })
-
-        } catch (e) {
-            console.log(e)
-        }
-
+        return await cloudinary.v2.uploader.upload(
+            file, {
+                folder: "trendy-it"
+            })
     }
 
     async deleteFile(file) {
-        try {
-            const deleteResponse = await cloudinary.v2.api.delete_resources(file)
-            return Object.values(deleteResponse.deleted)[0]
-
-        } catch (e) {
-            console.log(e)
-        }
-
+        const deleteResponse = await cloudinary.v2.api.delete_resources(file)
+        return Object.values(deleteResponse.deleted)[0]
     }
 }
 
