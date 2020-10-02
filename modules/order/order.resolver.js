a0c75d392b200d5caa1c236532f852f9546c2dad
Dmytro Didukh <jeriko.outback@gmail.com>
2020-10-03 00:50:21 +0300

    orders filter service updates #13

--- modules/order/order.resolver.js
+++ modules/order/order.resolver.js
@@ -1,7 +1,7 @@
 import orderService from './order.service';
 
 const orderQuery = {
-  getOrders: (_, args) => orderService.getOrders(args.filter, args.page),
+  getOrders: (_, args) => orderService.getOrders(args),
   getOrderById: (parent, args) => orderService.getOrderById(args.id)
 };
 
