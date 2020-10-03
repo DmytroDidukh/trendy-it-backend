fec2209fe089f9adc820dad2746d2de0f424e483
Dmytro Didukh <jeriko.outback@gmail.com>
2020-10-03 20:18:41 +0300

    prettier

--- modules/order/order.service.js
+++ modules/order/order.service.js
@@ -1,80 +1,80 @@
-import {Order, Product} from '../../models';
+import { Order, Product } from '../../models';
 import sendEmail from '../../utils/sendmail';
 import orderMailMessage from '../../utils/orderMailMessage';
 
 class OrderService {
-    async getOrders({filter, page}) {
-        const options = {
-            page: parseInt(page, 10) || 1,
-            limit: 20,
-            sort: '-createdAt',
-        };
-        const query = this.filterOrders(filter)
+  async getOrders({ filter, page }) {
+    const options = {
+      page: parseInt(page, 10) || 1,
+      limit: 20,
+      sort: '-createdAt'
+    };
+    const query = this.filterOrders(filter);
 
-        return await Order.paginate(query, options, (err, result) => ({
-            orders: result.docs,
-            pagination: {
-                totalDocs: result.totalDocs,
-                totalPages: result.totalPages,
-            }
-        }));
+    return await Order.paginate(query, options, (err, result) => ({
+      orders: result.docs,
+      pagination: {
+        totalDocs: result.totalDocs,
+        totalPages: result.totalPages
+      }
+    }));
+  }
+
+  getOrderById(id) {
+    return Order.findById(id);
+  }
+
+  async addOrder(data) {
+    const order = await new Order(data);
+    await order.save();
+
+    const message = {
+      from: process.env.MAIL_USER,
+      to: order.customer.email,
+      cc: [{ email: process.env.MAIL_CC }],
+      subject: 'Trendy IT',
+      html: orderMailMessage(order)
+    };
+
+    await sendEmail(message);
+
+    return order;
+  }
+
+  updateOrderStatus({ id, status }) {
+    return Order.findOneAndUpdate(
+      { _id: id },
+      { status: status },
+      { returnOriginal: false }
+    );
+  }
+
+  deleteOrder(id) {
+    return Order.findByIdAndRemove(id);
+  }
+
+  filterOrders(args = {}) {
+    const { search, status } = args;
+    const query = {};
+
+    if (status) {
+      query.status = status;
     }
 
-    getOrderById(id) {
-        return Order.findById(id);
+    if (!(!search || search.trim().length === 0)) {
+      query.$or = [
+        { 'customer.name': { $regex: new RegExp(search, 'i') } },
+        { 'customer.surname': { $regex: new RegExp(search, 'i') } },
+        { 'customer.email': { $regex: new RegExp(search, 'i') } },
+        { 'delivery.method': { $regex: new RegExp(search, 'i') } },
+        { connectionMethod: { $regex: new RegExp(search, 'i') } },
+        { paymentMethod: { $regex: new RegExp(search, 'i') } },
+        { orderId: { $regex: new RegExp(search, 'i') } }
+      ];
     }
 
-    async addOrder(data) {
-        const order = await new Order(data);
-        await order.save();
-
-        const message = {
-            from: process.env.MAIL_USER,
-            to: order.customer.email,
-            cc: [{email: process.env.MAIL_CC}],
-            subject: 'Trendy IT',
-            html: orderMailMessage(order)
-        };
-
-        await sendEmail(message);
-
-        return order;
-    }
-
-    updateOrderStatus({id, status}) {
-        return Order.findOneAndUpdate(
-            {_id: id},
-            {status: status},
-            {returnOriginal: false}
-        );
-    }
-
-    deleteOrder(id) {
-        return Order.findByIdAndRemove(id);
-    }
-
-    filterOrders(args = {}) {
-        const {search, status} = args;
-        const query = {};
-
-        if (status) {
-            query.status = status;
-        }
-
-        if (!(!search || search.trim().length === 0)) {
-            query.$or = [
-                {"customer.name": {$regex: new RegExp(search, 'i')}},
-                {"customer.surname": {$regex: new RegExp(search, 'i')}},
-                {"customer.email": {$regex: new RegExp(search, 'i')}},
-                {"delivery.method": {$regex: new RegExp(search, 'i')}},
-                {"connectionMethod": {$regex: new RegExp(search, 'i')}},
-                {"paymentMethod": {$regex: new RegExp(search, 'i')}},
-                {"orderId": {$regex: new RegExp(search, 'i')}},
-            ];
-        }
-
-        return query;
-    }
+    return query;
+  }
 }
 
 export default new OrderService();
--- modules/product/product.service.js
+++ modules/product/product.service.js
@@ -1,113 +1,123 @@
-import {Product} from '../../models';
+import { Product } from '../../models';
 import ImagesService from '../images/images.service';
 
 class ProductService {
-    async getProducts({filter, sort, page, limit}) {
-        const query = this.filterProducts(filter);
-        const options = {
-            page: parseInt(page, 10) || 1,
-            limit: parseInt(limit, 10) || 12,
-            sort: sort
-        };
+  async getProducts({ filter, sort, page, limit }) {
+    const query = this.filterProducts(filter);
+    const options = {
+      page: parseInt(page, 10) || 1,
+      limit: parseInt(limit, 10) || 12,
+      sort: sort
+    };
 
-        return await Product.paginate(query, options, (err, result) => ({
-            products: result.docs,
-            pagination: {
-                totalDocs: result.totalDocs,
-                currentPage: result.page,
-                totalPages: result.totalPages,
-                hasPrevPage: result.hasPrevPage,
-                hasNextPage: result.hasNextPage
-            }
-        }));
+    return await Product.paginate(query, options, (err, result) => ({
+      products: result.docs,
+      pagination: {
+        totalDocs: result.totalDocs,
+        currentPage: result.page,
+        totalPages: result.totalPages,
+        hasPrevPage: result.hasPrevPage,
+        hasNextPage: result.hasNextPage
+      }
+    }));
+  }
+
+  getProductById(id) {
+    return Product.findById(id);
+  }
+
+  addProduct(data) {
+    const product = new Product(data);
+    return product.save();
+  }
+
+  updateProduct({ id, product }) {
+    return Product.findByIdAndUpdate(
+      id,
+      { $set: { ...product } },
+      { new: true }
+    );
+  }
+
+  async deleteProduct(id) {
+    const currentProduct = await this.getProductById(id);
+    const { images } = currentProduct;
+
+    const imagesToDelete = [
+      images.slider,
+      ...Object.values(images.product).map((img) => img.publicId)
+    ].filter((val) => val);
+
+    await ImagesService.deleteImages(imagesToDelete);
+
+    return Product.findByIdAndRemove(id);
+  }
+
+  filterProducts(args = {}) {
+    const {
+      colors,
+      priceRange,
+      search,
+      hot,
+      available,
+      newItem,
+      sale,
+      toSlider,
+      isHomeQuery
+    } = args;
+
+    if (isHomeQuery) {
+      return { available: true, $or: [{ hot: true }, { toSlider: true }] };
     }
 
-    getProductById(id) {
-        return Product.findById(id);
+    const query = {};
+
+    if (hot) {
+      query.hot = hot;
     }
 
-    addProduct(data) {
-        const product = new Product(data);
-        return product.save();
+    if (toSlider) {
+      query.toSlider = toSlider;
     }
 
-    updateProduct({id, product}) {
-        return Product.findByIdAndUpdate(
-            id,
-            {$set: {...product}},
-            {new: true}
-        );
+    if (available) {
+      query.available = available;
     }
 
-    async deleteProduct(id) {
-        const currentProduct = await this.getProductById(id);
-        const {images} = currentProduct;
-
-        const imagesToDelete = [
-            images.slider,
-            ...Object.values(images.product).map((img) => img.publicId)
-        ].filter((val) => val);
-
-        await ImagesService.deleteImages(imagesToDelete);
-
-        return Product.findByIdAndRemove(id);
+    if (newItem) {
+      query.newItem = newItem;
     }
 
-    filterProducts(args = {}) {
-        const {colors, priceRange, search, hot, available, newItem, sale, toSlider, isHomeQuery} = args;
-
-        if (isHomeQuery) {
-            return {available: true, $or: [{hot: true}, {toSlider: true}]}
-        }
-
-        const query = {};
-
-        if (hot) {
-            query.hot = hot;
-        }
-
-        if (toSlider) {
-            query.toSlider = toSlider;
-        }
-
-        if (available) {
-            query.available = available;
-        }
-
-        if (newItem) {
-            query.newItem = newItem;
-        }
-
-        if (sale) {
-            query.sale = sale;
-        }
-
-        if (colors && colors.length) {
-            query.color = {$in: colors}
-        }
-
-        if (priceRange && priceRange.length) {
-            query.price = {
-                $gte: priceRange[0],
-                $lte: priceRange[1]
-            };
-        }
-
-        if (!(!search || search.trim().length === 0)) {
-            query.$or = [
-                {
-                    name: {$regex: new RegExp(search, 'i')}
-                },
-                {
-                    description: {
-                        $regex: new RegExp(search, 'i')
-                    }
-                }
-            ];
-        }
-
-        return query;
+    if (sale) {
+      query.sale = sale;
     }
+
+    if (colors && colors.length) {
+      query.color = { $in: colors };
+    }
+
+    if (priceRange && priceRange.length) {
+      query.price = {
+        $gte: priceRange[0],
+        $lte: priceRange[1]
+      };
+    }
+
+    if (!(!search || search.trim().length === 0)) {
+      query.$or = [
+        {
+          name: { $regex: new RegExp(search, 'i') }
+        },
+        {
+          description: {
+            $regex: new RegExp(search, 'i')
+          }
+        }
+      ];
+    }
+
+    return query;
+  }
 }
 
 export default new ProductService();
